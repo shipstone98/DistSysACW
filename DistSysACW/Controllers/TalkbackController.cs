7999e777fecd07c698e14e72de49d13695ad2108
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-17 15:38:13 +0000

    Added existing code

---
+++ DistSysACW/Controllers/TalkbackController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DistSysACW.Controllers
+{
+    public class TalkBackController : BaseController
+    {
+        /// <summary>
+        /// Constructs a TalkBack controller, taking the UserContext through dependency injection
+        /// </summary>
+        /// <param name="context">DbContext set as a service in Startup.cs and dependency injected</param>
+        public TalkBackController(Models.UserContext context) : base(context) { }
+
+
+        [ActionName("Hello")]
+        public string Get()
+        {
+            #region TASK1
+            // TODO: add api/talkback/hello response
+            #endregion
+        }
+
+        [ActionName("Sort")]
+        public IActionResult Get([FromQuery]int[] integers)
+        {
+            #region TASK1
+            // TODO: 
+            // sort the integers into ascending order
+            // send the integers back as the api/talkback/sort response
+            #endregion
+        }
+    }
+}
---
+++ DistSysACW/Middleware/AuthMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DistSysACW.Middleware
+{
+    public class AuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, Models.UserContext dbContext)
+        {
+            #region Task5
+            // TODO:  Find if a header ‘ApiKey’ exists, and if it does, check the database to determine if the given API Key is valid
+            //        Then set the correct roles for the User, using claims
+            #endregion
+
+            // Call the next delegate/middleware in the pipeline
+            await _next(context);
+        }
+
+    }
+}
