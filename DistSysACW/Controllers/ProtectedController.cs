949b4a6aade962c50440bfba37362172c59144e0
Christopher Shipstone <christopher.shipstone98@gmail.com>
2020-03-30 09:54:42 +0100

    Added Hello method for Task 9

--- DistSysACW/Controllers/ProtectedController.cs
+++ DistSysACW/Controllers/ProtectedController.cs
@@ -5,17 +5,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using DistSysACW.Models;
+
 namespace DistSysACW.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
-    public class ProtectedController : ControllerBase
+    [Route("api/[controller]/[action]")]
+    public class ProtectedController : BaseController
     {
+        public ProtectedController(UserContext context) : base(context) { }
+
         [ActionName("Hello")]
         [HttpGet]
         public IActionResult Hello([FromHeader] String apiKey)
         {
-            throw new NotImplementedException();
+            User user = UserDatabaseAccess.Get(this.Context, apiKey);
+            return user is null ? (IActionResult) this.BadRequest("ERROR: user not found") : this.Ok($"Hello {user.UserName}");
         }
 
         [ActionName("SHA1")]
